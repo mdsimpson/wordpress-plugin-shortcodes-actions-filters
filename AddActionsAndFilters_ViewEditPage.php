b3b5e6964514db48a18136ca613c76df00cbd944
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-12-23 20:18:30 +0000

    File reformat
    git-svn-id: http://plugins.svn.wordpress.org/add-actions-and-filters/trunk@1315306 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- AddActionsAndFilters_ViewEditPage.php
+++ AddActionsAndFilters_ViewEditPage.php
@@ -43,8 +43,9 @@ class AddActionsAndFilters_ViewEditPage
     /**
      * Add CodeMirror scripts for the code editor
      */
-    public function outputCodeMirrorScriptsAndCss() {
-            $libs = array(
+    public function outputCodeMirrorScriptsAndCss()
+    {
+        $libs = array(
             'lib/codemirror.js',
             'lib/codemirror.css',
             'addon/edit/matchbrackets.js',
@@ -58,13 +59,13 @@ class AddActionsAndFilters_ViewEditPage
         $baseUrl = $this->plugin->getPluginFileUrl('codemirror-5.9');
         foreach ($libs as $lib) {
             if (substr($lib, -3) == ".js") {
-            ?>
-            <script src="<?php echo "$baseUrl/$lib" ?>"></script>
-            <?php
+                ?>
+                <script src="<?php echo "$baseUrl/$lib" ?>"></script>
+                <?php
             } else if (substr($lib, -4) == ".css") {
-            ?>
-            <link rel="stylesheet" href="<?php echo "$baseUrl/$lib" ?>">
-            <?php
+                ?>
+                <link rel="stylesheet" href="<?php echo "$baseUrl/$lib" ?>">
+                <?php
             }
         }
     }
@@ -72,76 +73,82 @@ class AddActionsAndFilters_ViewEditPage
     /**
      * Add top header table
      */
-    public function outputHeader() {
-    ?>
+    public function outputHeader()
+    {
+        ?>
         <div class="wrap">
-        <table width="100%">
-            <tbody>
-            <tr>
-                <td align="left"><h2><?php _e('Code Editor', 'add-actions-and-filters'); ?></h2></td>
-                <td align="right">
-                    <a href="<?php echo get_admin_url() . 'admin.php?page=' . $this->plugin->getAdminPageSlug() ?>">
-                        <img width="128" height="50"
-                             src="<?php echo $this->plugin->getPluginFileUrl('img/icon-256x100.png') ?>">
-                    </a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+            <table width="100%">
+                <tbody>
+                <tr>
+                    <td align="left"><h2><?php _e('Code Editor', 'add-actions-and-filters'); ?></h2></td>
+                    <td align="right">
+                        <a href="<?php echo get_admin_url() . 'admin.php?page=' . $this->plugin->getAdminPageSlug() ?>">
+                            <img width="128" height="50"
+                                 src="<?php echo $this->plugin->getPluginFileUrl('img/icon-256x100.png') ?>">
+                        </a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         </div>
-    <?php
+        <?php
     }
 
     /**
      * Output the main contents of the page including the code editor and metadata fields
      * @param $item array created by AddActionsAndFilters_DataModel
      */
-    public function outputCodeEditor($item) {
-            ?>
-    <div class="wrap">
+    public function outputCodeEditor($item)
+    {
+        ?>
+        <div class="wrap">
 
-    <table width="100%">
-    <tbody>
-        <tr>
-            <td valign="top">
-                <label for="name">Name:</label>
-            </td>
-            <td valign="top">
-                <input id="name" type="text" value="<?php echo $item['name'] ?>" size="25"/>
-            </td>
-            <td valign="top">
-                <label for="description">Description:</label>
-            </td>
-            <td valign="top">
-                <textarea title="description" id="description" cols="80"><?php echo $item['description'] ?></textarea>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top">
-                <input type="checkbox" id="enabled" name="enabled" value="true" <?php if ($item['enabled']) echo 'checked' ?>><label for="enabled">Enabled</label>
-            </td>
-            <td valign="top">
-                <input type="checkbox" id="shortcode" name="shortcode" value="true" <?php if ($item['shortcode']) echo 'checked' ?>><label for="shortcode">Shortcode</label>
-            </td>
-        </tr>
-    </tbody>
-    </table>
+            <table width="100%">
+                <tbody>
+                <tr>
+                    <td valign="top">
+                        <label for="name">Name:</label>
+                    </td>
+                    <td valign="top">
+                        <input id="name" type="text" value="<?php echo $item['name'] ?>" size="25"/>
+                    </td>
+                    <td valign="top">
+                        <label for="description">Description:</label>
+                    </td>
+                    <td valign="top">
+                        <textarea title="description" id="description"
+                                  cols="80"><?php echo $item['description'] ?></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top">
+                        <input type="checkbox" id="enabled" name="enabled"
+                               value="true" <?php if ($item['enabled']) echo 'checked' ?>><label
+                            for="enabled">Enabled</label>
+                    </td>
+                    <td valign="top">
+                        <input type="checkbox" id="shortcode" name="shortcode"
+                               value="true" <?php if ($item['shortcode']) echo 'checked' ?>><label for="shortcode">Shortcode</label>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
 
 
-    <textarea title="code" id="codefield"><?php echo $item['code'] ?></textarea>
+            <textarea title="code" id="codefield"><?php echo $item['code'] ?></textarea>
 
 
-    <script>
-      var editor = CodeMirror.fromTextArea(document.getElementById("codefield"), {
-        lineNumbers: true,
-        matchBrackets: true,
-        mode: "text/x-php",
-        indentUnit: 4,
-        indentWithTabs: true
-      });
-    </script>
+            <script>
+                var editor = CodeMirror.fromTextArea(document.getElementById("codefield"), {
+                    lineNumbers: true,
+                    matchBrackets: true,
+                    mode: "text/x-php",
+                    indentUnit: 4,
+                    indentWithTabs: true
+                });
+            </script>
 
-    </div>
+        </div>
 
         <?php
         submit_button('Save', 'primary', 'savecode');
@@ -149,7 +156,8 @@ class AddActionsAndFilters_ViewEditPage
     }
 
 
-    public function old_display() {
+    public function old_display()
+    {
         // todo: change to new edit page code
         ?>
 
